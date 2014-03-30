c602210640074d8da543cba5ab6467a6ca71c657
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-03-31 04:38:14 +0900

    fix test command

--- test.js
+++ test.js
@@ -2,17 +2,21 @@ var fs = require("fs");
 var child_process = require('child_process');
 var assert = require('assert');
 
-process.env.HOOKSPATH = __dirname;
-child_process.spawn("bin/hookin.js")
-  .on("close", function(code) {
+process.env.HOOKSPATH = "/tmp";
+var hookin = child_process.spawn("bin/hookin.js");
+hookin.on("close", function(code) {
+  assert.equal(code, 0);
+  var postmerge = "" + fs.readFileSync(process.env.HOOKSPATH + "/post-merge");
+  assert.equal(postmerge.indexOf("#changed_files checker") > 0 , true);
+  var hookin = child_process.spawn("bin/hookin.js", ['test.js', 'npm test'])
+  hookin.on("close", function(code) {
     assert.equal(code, 0);
     var postmerge = "" + fs.readFileSync(process.env.HOOKSPATH + "/post-merge");
-    assert.equal(postmerge.indexOf("#changed_files checker") > 0 , true);
-    child_process.spawn("bin/hookin.js", ['test.js', 'npm test'])
-    .on("close", function(code) {
-      assert.equal(code, 0);
-      var postmerge = "" + fs.readFileSync(process.env.HOOKSPATH + "/post-merge");
-      assert.equal(postmerge.indexOf("# test.js") > 0 , true);
-      assert.equal(postmerge.indexOf("npm test") > 0 , true);
-    }).stdout.pipe(process.stdout);
-  }).stdout.pipe(process.stdout);
+    assert.equal(postmerge.indexOf("# test.js") > 0 , true);
+    assert.equal(postmerge.indexOf("npm test") > 0 , true);
+  });
+  hookin.stdout.pipe(process.stdout);
+  hookin.stderr.pipe(process.stderr);
+});
+hookin.stdout.pipe(process.stdout);
+hookin.stderr.pipe(process.stderr);
